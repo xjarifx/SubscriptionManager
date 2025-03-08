1b4499f731fe8b2ede87606e0f34e1be60b4329e
jarif <xjarifx@gmail.com>
2025-03-08 22:19:07 +0600

    .

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -65,15 +65,27 @@ const subscriptionSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+subscriptionSchema.pre("save", function (next) {
+  if (!this.renewalDate) {
+    const renewalPeriods = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
 
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriods[this.frequency]
+    );
+  }
 
+  if (this.renewalDate < new Date()) {
+    this.status = "expired";
+  }
 
-
-
-
-
-
-
+  next();
+});
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
