ac1c2f74dd745b31a48ac2d126dc828fdec51dd5
jarif <xjarifx@gmail.com>
2025-03-09 10:09:44 +0600

    .

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import PORT from "./config/env.js";
 import userRouter from "./routes/users.routers.js";
 import authRouter from "./routes/auth.routers.js";
 import subscriptionRouter from "./routes/subscriptions.routers.js";
 import connectDB from "./database/mongodb.js";
-import { PORT } from "./config/env.js";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.listen(PORT, async () => {
-  await connectDB();
   console.log(`${process.env.SERVER_URL}${PORT}`);
+  await connectDB();
 });
